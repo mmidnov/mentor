05339ad89824875753d19160978f09580786318a
mmidinov <mmidinov13@gmail.com>
2023-09-08 18:20:02 +0600

    mentor

--- ads/views.py
+++ ads/views.py
@@ -42,7 +42,7 @@ class AdsListView(ListView):
     # 6
     # all_ads = Ads.objects.filter(price__in=[200,3500,343,2345])
 class AdsDetailView(DetailView):
-    templete_name = 'retrieve_ad.html'
+    template_name = 'retrieve_ad.html'
     queryset = Ads.objects.all()
     context_object_name = 'ad'
 
@@ -70,44 +70,3 @@ class AdsCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('ads_list')
-
-
-
-def created_at(request):
-    if request.method == 'POST':
-        form = AdForm(request.POST,request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('ads_list')
-    else:
-        form_of_ad = AdForm()
-    return render(request, 'create_ad.html',{'form_of_ad':form_of_ad})
-
-def update_ad(request,pk):
-    ad = get_object_or_404(Ads, id=pk)
-    if request.method == 'POST':
-        form = AdForm(request.POST,request.FILES, instance=ad)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('<h1>Success edited</h1>')
-        else:
-            return HttpResponse('<h1>Error edited</h1>')
-
-    else:
-        form_of_ad = AdForm(instance=ad)
-        return render(request, 'update_ad.html',{'form_of_ad':form_of_ad})
-
-
-def retrieve_ad(request,pk):
-    # ad = get_object_or_404(Ads,id=pk)
-    ad = Ads.objects.get(id=pk)
-    context ={
-        "ad": ad
-    }
-    return render(request, 'retrieve_ad.html', context=context)
-
-def delete_ad(request,pk):
-    ad = Ads.objects.get(id=pk)
-    ad.delete()
-    messages.success(request,'Обьект успешно удален')
-    return redirect('ads_list')
